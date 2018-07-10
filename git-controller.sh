62aa8809babe822ffc8116d4a34b72d953394d40
Furkan Mataraci <furkan@ottoo.com>
2018-07-10 23:52:36 +0300

    Changes made at git-controller to restart the flask app

--- git-controller.sh
+++ git-controller.sh
@@ -2,6 +2,10 @@
 #Refer to the git-controller.service
 while [ 1 ];
 do 
-    sleep 1;
-    git pull ;
+    sleep 1 
+    result=`git pull`
+    if [ "$result" != "Already up-to-date."];
+    then
+        systemctl restart flask-app
+    fi
 done
